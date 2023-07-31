10ca517e2a77430ca2cbab3a3df2846e7ccb0405
thehassanshaikh <thehasanshaikh@gmail.com>
2023-07-31 19:31:42 +0530

    added hero section

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+const Header = ()=>{
+    return(
+        <div className="grid grid-cols-2 gap-4 container mx-auto">
+            <div className="flex items-center justify-center">
+                <h2 className="text-2xl font-semibold">View the Space with us.</h2>
+            </div>
+            <div>
+                <img className=" mt-6 rounded-3xl" src="https://images.unsplash.com/photo-1517976384346-3136801d605d" alt="" />
+            </div>
+        </div>
+    )
+}
+
+export  default Header
